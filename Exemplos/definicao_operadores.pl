cce8f0c85db3a9fedf87c2adba300b5a4c990a8a
Lariterrinha <lalaterrinha14@gmail.com>
2025-03-27 19:01:28 -0300

    Add: Codigos de Exemplo

---
+++ Exemplos/definicao_operadores.pl
@@ -0,0 +1,9 @@
+% *** Exemplo: definicao de novos operadores
+
+%tem(pedro, informacoes).
+ :- op(600, xfx, tem).
+pedro tem informacoes.
+maria tem informacoes.
+
+% tem(maria,X)
+% X tem informacoes
